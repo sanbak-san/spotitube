2be7c61071a86e832378cb5b88c73b2bfea5a66a
san <nafets.a@hotmail.com>
2019-03-11 11:41:30 +0100

    added gitignore

--- src/main/java/net/sanstech/LoginResource.java
+++ src/main/java/net/sanstech/LoginResource.java
@@ -5,14 +5,14 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/login")
 public class LoginResource {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public User loginUser(){
-
-        return User;
+    public Response loginUser(){
+        return Response.ok().entity(new Token()).build();
     }
 }
---
+++ src/main/java/net/sanstech/Token.java
@@ -0,0 +1,11 @@
+package net.sanstech;
+
+public class Token {
+    private String token;
+    private String user;
+
+    public Token() {
+        this.token = "123-456-789";
+        this.user = "harry";
+    }
+}
