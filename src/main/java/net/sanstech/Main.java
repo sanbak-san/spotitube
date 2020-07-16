1224e1430568933b69c066c72ed19786ddf2ac74
stefananbeek <nafets.a@hotmail.com>
2020-07-16 12:15:47 +0200

    Sonarcloud issues

--- src/main/java/net/sanstech/Main.java
+++ src/main/java/net/sanstech/Main.java
@@ -2,10 +2,6 @@ package net.sanstech;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-        } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
+        // Empty main method
     }
 }
