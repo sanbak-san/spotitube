8cf5c1a7c5f9c850f19fb04c88bd9aaaa8c5f4a3
stefananbeek <nafets.a@hotmail.com>
2020-07-16 11:59:24 +0200

    Sonarcloud issue

--- src/main/java/net/sanstech/Main.java
+++ src/main/java/net/sanstech/Main.java
@@ -1,7 +1,5 @@
 package net.sanstech;
 
-import java.sql.*;
-
 public class Main {
     public static void main(String[] args) {
         try {
@@ -9,23 +7,5 @@ public class Main {
         } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
             e.printStackTrace();
         }
-
-        try (
-                Connection connection = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:3306/spotitube", "root", "");
-                PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM ACCOUNT WHERE user=?");
-        ) {
-            preparedStatement.setString(1, "harry");
-            ResultSet resultSet = preparedStatement.executeQuery();
-            System.out.println("fsdjfbdjbf");
-
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString("password"));
-                System.out.println("fsdjfbdjbf");
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
     }
 }
